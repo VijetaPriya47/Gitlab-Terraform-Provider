3027ed1e0861b405ef81f13b3e58fce5aff96ed2
Heidi Berry <heidi.berry@coop.co.uk>
2024-05-23 11:24:43 +0000

    Correct examples for resource project_job_token_scopes
    Changelog: fixed
    Subsystem: resource/gitlab_project_job_token_scopes

--- examples/resources/gitlab_project_job_token_scopes/resource.tf
+++ examples/resources/gitlab_project_job_token_scopes/resource.tf
@@ -1,15 +1,15 @@
 resource "gitlab_project_job_token_scopes" "allowed_single_project" {
-  project            = "gitlab-org/gitlab"
+  project_id         = 111
   target_project_ids = [123]
 }
 
 resource "gitlab_project_job_token_scopes" "allowed_multiple_project" {
-  project            = "gitlab-org/gitlab"
+  project_id         = 111
   target_project_ids = [123, 456, 789]
 }
 
 # This will remove all job token scopes, even if added outside of TF.
 resource "gitlab_project_job_token_scopes" "explicit_deny" {
-  project            = "gitlab-org/gitlab"
+  project_id         = 111
   target_project_ids = []
 }
