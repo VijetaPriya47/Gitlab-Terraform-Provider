8b871525a2499dedf0fbc0889387b8498a30f2b5
Heidi Berry <heidi.berry@coop.co.uk>
2024-11-19 10:56:50 +0000

    Add more detail to service account examples. Make required permissions on gitlab.com more obvious.

--- examples/resources/gitlab_group_service_account/resource.tf
+++ examples/resources/gitlab_group_service_account/resource.tf
@@ -1,11 +1,28 @@
+# This must be a top-level group
 resource "gitlab_group" "example" {
   name        = "example"
   path        = "example"
   description = "An example group"
 }
 
-resource "gitlab_group_service_account" "example-sa" {
+# The service account against the top-level group
+resource "gitlab_group_service_account" "example_sa" {
   group    = gitlab_group.example.id
   name     = "example-name"
   username = "example-username"
+}
+
+# Group to assign the service account to. Can be the same top-level group resource as above, or a subgroup of that group.
+resource "gitlab_group" "example_subgroup" {
+  name        = "subgroup"
+  path        = "example/subgroup"
+  description = "An example subgroup"
+}
+
+# To assign the service account to a group
+resource "gitlab_group_membership" "example_membership" {
+  group_id     = gitlab_group.example_subgroup.id
+  user_id      = gitlab_group_service_account.example_sa.service_account_id
+  access_level = "developer"
+  expires_at   = "2020-03-14"
 }
--- examples/resources/gitlab_group_service_account_access_token/resource.tf
+++ examples/resources/gitlab_group_service_account_access_token/resource.tf
@@ -1,19 +1,30 @@
+# This must be a top-level group
 resource "gitlab_group" "example" {
   name        = "example"
   path        = "example"
   description = "An example group"
 }
 
-resource "gitlab_group_service_account" "example-sa" {
+# The service account against the top-level group
+resource "gitlab_group_service_account" "example_sa" {
   group    = gitlab_group.example.id
   name     = "example-name"
   username = "example-username"
 }
 
-resource "gitlab_group_service_account_access_token" "example-sa-token" {
+# To assign the service account to a group
+resource "gitlab_group_membership" "example_membership" {
+  group_id     = gitlab_group.example.id
+  user_id      = gitlab_group_service_account.example_sa.service_account_id
+  access_level = "developer"
+  expires_at   = "2020-03-14"
+}
+
+# The service account access token
+resource "gitlab_group_service_account_access_token" "example_sa_token" {
   group      = gitlab_group.example.id
-  user_id    = gitlab_group_service_account.example-sa.service_account_id
-  name       = "Example personal access token"
+  user_id    = gitlab_group_service_account.example_sa.service_account_id
+  name       = "Example service account access token"
   expires_at = "2020-03-14"
 
   scopes = ["api"]
