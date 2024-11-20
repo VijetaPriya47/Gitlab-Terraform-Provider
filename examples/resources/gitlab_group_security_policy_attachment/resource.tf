72457495cad307146cb1200318c447a1fb6bbe2a
Olivier Dupré <odupre@gitlab.com>
2024-11-20 21:36:09 +0000

    Doc: scope policies to a framework
    Changelog: Improvements
    Subsystem: resource/gitlab_group_security_policy_attachment

--- examples/resources/gitlab_group_security_policy_attachment/resource.tf
+++ examples/resources/gitlab_group_security_policy_attachment/resource.tf
@@ -5,7 +5,7 @@ resource "gitlab_group_security_policy_attachment" "foo" {
 }
 
 
-# Or you can use Terraform to create a new project, add a policy to that project,
+# Or Terraform can create a new project, add a policy to that project,
 # then attach that policy project to other groups.
 resource "gitlab_project" "my-policy-project" {
   name = "security-policy-project"
@@ -35,12 +35,17 @@ approval_settings:
     require_password_to_approve: false
 fallback_behavior:
     fail: closed
+policy_scope:
+  compliance_frameworks:
+  - id: 1010101
+  - id: 0101010
 actions:
 - type: send_bot_message
     enabled: true
 EOT
 }
 
+# Multiple policies can be attached to a single project by repeating this resource or using a `for_each`
 resource "gitlab_group_security_policy_attachment" "my-policy" {
   group          = 1234
   policy_project = gitlab_project.my-policy-project.id
