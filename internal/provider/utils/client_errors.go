2c09710770fe7baacb993d443f2ee3aef47c8b5c
Yogesh <yogesh@lonkar.org>
2025-01-27 00:20:34 +0000

    Bugfix gitlab_group_variable resource

--- internal/provider/utils/client_errors.go
+++ internal/provider/utils/client_errors.go
@@ -7,14 +7,13 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	gitlab "gitlab.com/gitlab-org/api/client-go"
 )
 
-func AugmentVariableClientError(ctx context.Context, masked bool, err error, d diag.Diagnostics) bool {
+func AugmentVariableClientError(ctx context.Context, masked bool, err error) (bool, error) {
 	if !masked {
-		return false
+		return false, nil
 	}
 
 	var httpErr *gitlab.ErrorResponse
@@ -23,9 +22,8 @@ func AugmentVariableClientError(ctx context.Context, masked bool, err error, d d
 			strings.Contains(httpErr.Message, "value") &&
 			strings.Contains(httpErr.Message, "invalid") {
 			tflog.Error(ctx, fmt.Sprintf("[ERROR] %v", err))
-			d.AddError("Invalid value for a masked variable. Check the masked variable requirements: https://docs.gitlab.com/ee/ci/variables/#masked-variable-requirements", err.Error())
-			return true
+			return true, err
 		}
 	}
-	return false
+	return false, nil
 }
