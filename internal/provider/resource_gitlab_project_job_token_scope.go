cd6c9e62237794406e1595d127ae7ed5c0dcbc08
Roel de Cort <roel@decort.tech>
2025-02-02 17:14:29 +0000

    Add support for `groupd_id`
    Changelog: IMPROVEMENTS
    Subsystem: resource/gitlab_project_job_token_scope

--- internal/provider/resource_gitlab_project_job_token_scope.go
+++ internal/provider/resource_gitlab_project_job_token_scope.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strconv"
 
+	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
@@ -15,16 +16,21 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
-	"gitlab.com/gitlab-org/api/client-go"
-	"gitlab.com/gitlab-org/terraform-provider-gitlab/internal/provider/api"
+	gitlab "gitlab.com/gitlab-org/api/client-go"
 	"gitlab.com/gitlab-org/terraform-provider-gitlab/internal/provider/utils"
 )
 
 // Ensure provider defined types fully satisfy framework interfaces
 var (
-	_ resource.Resource                = &gitlabProjectJobTokenScopeResource{}
-	_ resource.ResourceWithConfigure   = &gitlabProjectJobTokenScopeResource{}
-	_ resource.ResourceWithImportState = &gitlabProjectJobTokenScopeResource{}
+	_ resource.Resource                 = &gitlabProjectJobTokenScopeResource{}
+	_ resource.ResourceWithConfigure    = &gitlabProjectJobTokenScopeResource{}
+	_ resource.ResourceWithImportState  = &gitlabProjectJobTokenScopeResource{}
+	_ resource.ResourceWithUpgradeState = &gitlabProjectJobTokenScopeResource{}
+)
+
+const (
+	targetTypeProject = "project"
+	targetTypeGroup   = "group"
 )
 
 func init() {
@@ -46,13 +52,21 @@ type gitlabProjectJobTokenScopeResourceModel struct {
 	Id              types.String `tfsdk:"id"`
 	Project         types.String `tfsdk:"project"`
 	TargetProjectID types.Int64  `tfsdk:"target_project_id"`
+	TargetGroupID   types.Int64  `tfsdk:"target_group_id"`
+}
+
+// gitlabProjectJobTokenScopeResourceModelSchema0 represents the V0 version of the resource schema
+type gitlabProjectJobTokenScopeResourceModelSchema0 struct {
+	Id              types.String `tfsdk:"id"`
+	Project         types.String `tfsdk:"project"`
+	TargetProjectID types.Int64  `tfsdk:"target_project_id"`
 }
 
 func (r *gitlabProjectJobTokenScopeResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
 	resp.TypeName = req.ProviderTypeName + "_project_job_token_scope"
 }
 
-func (r *gitlabProjectJobTokenScopeResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+func (d *gitlabProjectJobTokenScopeResource) getV1Schema(resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		MarkdownDescription: `The ` + "`gitlab_project_job_token_scope`" + ` resource allows to manage the CI/CD Job Token scope in a project.
 Any projects added to the CI/CD Job Token scope outside of TF will be untouched by the resource.
@@ -75,14 +89,34 @@ Any projects added to the CI/CD Job Token scope outside of TF will be untouched 
 			},
 			"target_project_id": schema.Int64Attribute{
 				MarkdownDescription: "The ID of the project that is in the CI/CD job token inbound allowlist.",
-				Required:            true,
+				Optional:            true,
 				PlanModifiers: []planmodifier.Int64{
 					int64planmodifier.RequiresReplace(),
 					int64planmodifier.UseStateForUnknown(),
 				},
+				Validators: []validator.Int64{
+					int64validator.ExactlyOneOf(path.MatchRoot("target_group_id")),
+				},
+			},
+			"target_group_id": schema.Int64Attribute{
+				MarkdownDescription: "The ID of the group that is in the CI/CD job token inbound allowlist.",
+				Optional:            true,
+				PlanModifiers: []planmodifier.Int64{
+					int64planmodifier.RequiresReplace(),
+					int64planmodifier.UseStateForUnknown(),
+				},
+				Validators: []validator.Int64{
+					int64validator.ExactlyOneOf(path.MatchRoot("target_project_id")),
+				},
 			},
 		},
+		Version: 1,
 	}
+}
+
+// Schema returns the schema for the resource.
+func (r *gitlabProjectJobTokenScopeResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	r.getV1Schema(resp)
 }
 
 // Configure adds the provider configured client to the resource.
@@ -109,26 +143,47 @@ func (r *gitlabProjectJobTokenScopeResource) Create(ctx context.Context, req res
 
 	// local copies of plan arguments
 	projectID := data.Project.ValueString()
-	targetProjectID := int(data.TargetProjectID.ValueInt64())
 
-	options := &gitlab.JobTokenInboundAllowOptions{
-		TargetProjectID: gitlab.Ptr(targetProjectID),
-	}
+	var targetIDStr string
+	var targetType string
 
-	addTokenResponse, _, err := r.client.JobTokenScope.AddProjectToJobScopeAllowList(projectID, options, gitlab.WithContext(ctx))
-	if err != nil {
-		resp.Diagnostics.AddError("GitLab API error occurred", fmt.Sprintf("Unable to add the target project to CI/CD Job Token inbound allowlist: %s", err.Error()))
-		return
+	// Add the target project or group to the CI/CD Job Token inbound allowlist
+	switch {
+	case !data.TargetProjectID.IsNull():
+		targetID := int(data.TargetProjectID.ValueInt64())
+		options := &gitlab.JobTokenInboundAllowOptions{TargetProjectID: gitlab.Ptr(targetID)}
+		addTokenResponse, _, err := r.client.JobTokenScope.AddProjectToJobScopeAllowList(projectID, options, gitlab.WithContext(ctx))
+
+		if err != nil {
+			resp.Diagnostics.AddError("GitLab API error occurred", fmt.Sprintf("Unable to add the target project to CI/CD Job Token inbound allowlist: %s", err.Error()))
+			return
+		}
+		targetIDStr = strconv.Itoa(addTokenResponse.TargetProjectID)
+		targetType = targetTypeProject
+
+		// Log the creation of the resource
+		tflog.Debug(ctx, "Added the target project to CI/CD Job Token inbound allowlist", map[string]interface{}{
+			"project": projectID, "target_project": targetIDStr,
+		})
+
+	case !data.TargetGroupID.IsNull():
+		targetID := int(data.TargetGroupID.ValueInt64())
+		options := &gitlab.AddGroupToJobTokenAllowlistOptions{TargetGroupID: gitlab.Ptr(targetID)}
+		addTokenResponse, _, err := r.client.JobTokenScope.AddGroupToJobTokenAllowlist(projectID, options, gitlab.WithContext(ctx))
+		if err != nil {
+			resp.Diagnostics.AddError("GitLab API error occurred", fmt.Sprintf("Unable to add the target group to CI/CD Job Token inbound allowlist: %s", err.Error()))
+			return
+		}
+		targetIDStr = strconv.Itoa(addTokenResponse.TargetGroupID)
+		targetType = targetTypeGroup
+		// Log the creation of the resource
+		tflog.Debug(ctx, "Added the target group to CI/CD Job Token inbound allowlist", map[string]interface{}{
+			"project": projectID, "target_group": targetIDStr,
+		})
 	}
 
 	// Create resource ID and persist in state model
-	targetProjectIdStr := strconv.Itoa(addTokenResponse.TargetProjectID)
-	data.Id = types.StringValue(utils.BuildTwoPartID(&projectID, &targetProjectIdStr))
-
-	// Log the creation of the resource
-	tflog.Debug(ctx, "Added the target project to CI/CD Job Token inbound allowlist", map[string]interface{}{
-		"project": projectID, "target_project": targetProjectID,
-	})
+	data.Id = types.StringValue(fmt.Sprintf("%s:%s:%s", projectID, targetType, targetIDStr))
 
 	// Save data into Terraform state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
@@ -145,42 +200,34 @@ func (r *gitlabProjectJobTokenScopeResource) Read(ctx context.Context, req resou
 		return
 	}
 
-	// read all information for refresh from resource id
-	projectID, targetProjectIdStr, err := utils.ParseTwoPartID(data.Id.ValueString())
+	projectID, targetType, _, err := utils.ParseThreePartID(data.Id.ValueString())
+
 	if err != nil {
-		resp.Diagnostics.AddError(
-			"Invalid resource ID format",
-			fmt.Sprintf("The resource ID '%s' has an invalid format. It should be '<project>:<target-project-id>'. Error: %s", data.Id.ValueString(), err.Error()),
-		)
+		resp.Diagnostics.AddError("Invalid resource ID format", fmt.Sprintf("The resource ID '%s' has an invalid format. It should be '<project>:<target-id>'. Error: %s", data.Id.ValueString(), err.Error()))
 		return
 	}
 
-	targetProjectID, err := strconv.Atoi(targetProjectIdStr)
+	var found bool
+
+	// Check which type of target exists
+	switch targetType {
+	case targetTypeProject:
+		targetID := int(data.TargetProjectID.ValueInt64())
+		err, found = findProjectJobTokenInboundAllowlist(r.client, ctx, projectID, targetID)
+	case targetTypeGroup:
+		targetID := int(data.TargetGroupID.ValueInt64())
+		err, found = findGroupJobTokenInboundAllowlist(r.client, ctx, projectID, targetID)
+	}
 	if err != nil {
-		resp.Diagnostics.AddError(
-			"Invalid resource ID format",
-			fmt.Sprintf("target-project-id %s cannot be converted to int", targetProjectIdStr),
-		)
+		resp.Diagnostics.AddError("GitLab API error occurred", fmt.Sprintf("Unable to read project allowlist: %s", err.Error()))
 		return
 	}
 
-	// Read CI/CD Job Token inbound allowlist
-	err, found := findProjectJobTokenInboundAllowlist(r.client, ctx, projectID, targetProjectID)
-
-	if err != nil {
-		resp.Diagnostics.AddError("GitLab API error occured", err.Error())
-		return
-	}
 	if !found {
-		tflog.Debug(ctx, fmt.Sprintf("The target project (%d) not found in the CI/CD Job Token inbound allowlist, removing from state", targetProjectID))
 		resp.State.RemoveResource(ctx)
 		return
 	}
 
-	data.Project = types.StringValue(projectID)
-	data.TargetProjectID = types.Int64Value(int64(targetProjectID))
-
-	// Save updated data into Terraform state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
@@ -200,45 +247,162 @@ func (r *gitlabProjectJobTokenScopeResource) Delete(ctx context.Context, req res
 		return
 	}
 
-	// read all information for refresh from resource id
-	projectID, targetProjectIdStr, err := utils.ParseTwoPartID(data.Id.ValueString())
+	projectID, targetType, _, err := utils.ParseThreePartID(data.Id.ValueString())
+
 	if err != nil {
-		resp.Diagnostics.AddError(
-			"Invalid resource ID format",
-			fmt.Sprintf("The resource ID '%s' has an invalid format. It should be '<project>:<target-project-id>'. Error: %s", data.Id.ValueString(), err.Error()),
-		)
+		resp.Diagnostics.AddError("Invalid resource ID format", fmt.Sprintf("The resource ID '%s' has an invalid format. Error: %s", data.Id.ValueString(), err.Error()))
 		return
 	}
 
-	targetProjectID, err := strconv.Atoi(targetProjectIdStr)
-	if err != nil {
-		resp.Diagnostics.AddError(
-			"Invalid resource ID format",
-			fmt.Sprintf("target-project-id %s cannot be converted to int", targetProjectIdStr),
-		)
-		return
+	switch targetType {
+	case targetTypeProject:
+		targetID := int(data.TargetProjectID.ValueInt64())
+		_, err = r.client.JobTokenScope.RemoveProjectFromJobScopeAllowList(projectID, targetID, gitlab.WithContext(ctx))
+	case targetTypeGroup:
+		targetID := int(data.TargetGroupID.ValueInt64())
+		_, err = r.client.JobTokenScope.RemoveGroupFromJobTokenAllowlist(projectID, targetID, gitlab.WithContext(ctx))
 	}
 
-	_, err = r.client.JobTokenScope.RemoveProjectFromJobScopeAllowList(projectID, targetProjectID, gitlab.WithContext(ctx))
 	if err != nil {
-		if api.Is404(err) {
-			tflog.Debug(ctx, "project does not exist, removing resource from state", map[string]interface{}{
-				"project":        projectID,
-				"target_project": targetProjectIdStr,
-			})
-			resp.State.RemoveResource(ctx)
-			return
-		}
-		resp.Diagnostics.AddError("GitLab API error occurred", fmt.Sprintf("Unable to remove the target project to CI/CD Job Token inbound allowlist: %s", err.Error()))
+		resp.Diagnostics.AddError("GitLab API error occurred", fmt.Sprintf("Unable to remove target from CI/CD Job Token inbound allowlist: %s", err.Error()))
 		return
+	}
+}
+
+// GetV0Schema returns the schema for the V0 version of the resource
+func (r *gitlabProjectJobTokenScopeResource) getV0Schema() schema.Schema {
+	return schema.Schema{
+		MarkdownDescription: `The ` + "`gitlab_project_job_token_scope`" + ` resource allows to manage the CI/CD Job Token scope in a project.
+Any projects added to the CI/CD Job Token scope outside of TF will be untouched by the resource.
+
+~> Conflicts with the use of ` + "`gitlab_project_job_token_scopes`" + ` when used on the same project. Use one or the other to ensure the desired state.
+
+**Upstream API**: [GitLab REST API docs](https://docs.gitlab.com/ee/api/project_job_token_scopes.html)`,
+
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "The ID of this Terraform resource. In the format of `<project>:<target-project-id>`.",
+				PlanModifiers:       []planmodifier.String{stringplanmodifier.UseStateForUnknown()},
+			},
+			"project": schema.StringAttribute{
+				MarkdownDescription: "The ID or full path of the project.",
+				Required:            true,
+				PlanModifiers:       []planmodifier.String{stringplanmodifier.RequiresReplace()},
+				Validators:          []validator.String{stringvalidator.LengthAtLeast(1)},
+			},
+			"target_project_id": schema.Int64Attribute{
+				MarkdownDescription: "The ID of the project that is in the CI/CD job token inbound allowlist.",
+				Required:            true, // Changed from Optional to Required since it was the only option in V0
+				PlanModifiers: []planmodifier.Int64{
+					int64planmodifier.RequiresReplace(),
+					int64planmodifier.UseStateForUnknown(),
+				},
+			},
+		},
+		Version: 0,
+	}
+}
+
+// UpgradeState upgrades the resource state from V0 to V1 by adding the target type to the ID.
+func resourceGitlabProjectJobTokenScopeStateUpgradeV0ToV1(ctx context.Context, data *gitlabProjectJobTokenScopeResourceModelSchema0) (*gitlabProjectJobTokenScopeResourceModel, error) {
+	projectID, targetIDStr, err := utils.ParseTwoPartID(data.Id.ValueString())
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse two part ID: %s", err.Error())
+	}
+
+	// Target type will always be project because v0 only supported projects
+	targetType := targetTypeProject
+
+	tflog.Debug(ctx, "migrating state from V0 to V1", map[string]interface{}{
+		"old_id": data.Id.ValueString(),
+		"type":   targetType,
+	})
+
+	newID := fmt.Sprintf("%s:%s:%s", projectID, targetType, targetIDStr)
+
+	newData := &gitlabProjectJobTokenScopeResourceModel{
+		Id:              types.StringValue(newID),
+		Project:         data.Project,
+		TargetProjectID: data.TargetProjectID,
+	}
+	return newData, nil
+}
+
+// UpgradeState upgrades the resource state from V0 to V1
+func (r *gitlabProjectJobTokenScopeResource) UpgradeState(ctx context.Context) map[int64]resource.StateUpgrader {
+	schema := r.getV0Schema()
+
+	return map[int64]resource.StateUpgrader{
+		0: {
+			PriorSchema: &schema,
+			StateUpgrader: func(ctx context.Context, req resource.UpgradeStateRequest, resp *resource.UpgradeStateResponse) {
+				var data gitlabProjectJobTokenScopeResourceModelSchema0
+				resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+				if resp.Diagnostics.HasError() {
+					return
+				}
+				newData, err := resourceGitlabProjectJobTokenScopeStateUpgradeV0ToV1(ctx, &data)
+				if err != nil {
+					resp.Diagnostics.AddError("Unable to upgrade state", err.Error())
+					return
+				}
+				resp.Diagnostics.Append(resp.State.Set(ctx, &newData)...)
+			},
+		},
 	}
 }
 
 // ImportState imports the resource into the Terraform state.
 func (r *gitlabProjectJobTokenScopeResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+	// Split the import ID into its components and try to parse it as a three part ID
+	projectID, targetType, targetIDStr, err := utils.ParseThreePartID(req.ID)
+	if err != nil {
+		// If the three part ID fails, try to parse it as a two part ID
+		projectID, targetIDStr, err = utils.ParseTwoPartID(req.ID)
+		if err != nil {
+			resp.Diagnostics.AddError(
+				"Invalid import ID format",
+				fmt.Sprintf("The resource ID must be in the format 'project:type:target_id' or the legacy format 'project:target_id'. Got: %s. Error: %s", req.ID, err.Error()),
+			)
+			return
+		}
+		// For legacy IDs, assume the target type is project (as we do not have the group target in V0)
+		targetType = targetTypeProject
+	}
+
+	// Convert target ID to int for validation
+	targetID, err := strconv.Atoi(targetIDStr)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Invalid target ID",
+			fmt.Sprintf("Failed to parse target ID '%s' as integer: %s", targetIDStr, err.Error()),
+		)
+		return
+	}
+
+	// Create the new three part ID
+	newID := fmt.Sprintf("%s:%s:%s", projectID, targetType, targetIDStr)
+
+	// Set the basic fields
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), newID)...)
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("project"), projectID)...)
+
+	// Set the appropriate target ID based on type
+	switch targetType {
+	case targetTypeProject:
+		resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("target_project_id"), targetID)...)
+	case targetTypeGroup:
+		resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("target_group_id"), targetID)...)
+	default:
+		resp.Diagnostics.AddError(
+			"Invalid target type",
+			fmt.Sprintf("Target type must be either 'project' or 'group'. Got: %s", targetType),
+		)
+	}
 }
 
+// findProjectJobTokenInboundAllowlist finds the target project in the CI/CD Job Token inbound allowlist
 func findProjectJobTokenInboundAllowlist(client *gitlab.Client, ctx context.Context, projectID string, targetProjectID int) (error, bool) {
 	options := gitlab.GetJobTokenInboundAllowListOptions{
 		ListOptions: gitlab.ListOptions{
@@ -255,6 +419,33 @@ func findProjectJobTokenInboundAllowlist(client *gitlab.Client, ctx context.Cont
 
 		for i := range paginatedProjects {
 			if paginatedProjects[i].ID == targetProjectID {
+				return nil, true
+			}
+		}
+
+		options.Page = resp.NextPage
+	}
+
+	return nil, false
+}
+
+// findGroupJobTokenInboundAllowlist finds the target group in the CI/CD Job Token inbound allowlist
+func findGroupJobTokenInboundAllowlist(client *gitlab.Client, ctx context.Context, projectID string, targetGroupID int) (error, bool) {
+	options := gitlab.GetJobTokenAllowlistGroupsOptions{
+		ListOptions: gitlab.ListOptions{
+			PerPage: 20,
+			Page:    1,
+		},
+	}
+
+	for options.Page != 0 {
+		paginatedGroups, resp, err := client.JobTokenScope.GetJobTokenAllowlistGroups(projectID, &options, gitlab.WithContext(ctx))
+		if err != nil {
+			return fmt.Errorf("unable to read CI/CD Job Token inbound groups_allowlist. %s", err), false
+		}
+
+		for i := range paginatedGroups {
+			if paginatedGroups[i].ID == targetGroupID {
 				return nil, true
 			}
 		}
