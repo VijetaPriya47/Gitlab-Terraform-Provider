2e6418b512340496f7ece5f00b8ccd0769b01a3c
Heidi Berry <heidi.berry@coop.co.uk>
2025-05-27 23:36:06 +0100

    Add support for `expires_at`, allowing expiring project deploy keys
    Changelog: IMPROVEMENTS
    Subsystem: resource/gitlab_deploy_key

--- examples/resources/gitlab_deploy_key/resource.tf
+++ examples/resources/gitlab_deploy_key/resource.tf
@@ -1,5 +1,14 @@
+// No expiry
 resource "gitlab_deploy_key" "example" {
   project = "example/deploying"
   title   = "Example deploy key"
   key     = "ssh-ed25519 AAAA..."
+}
+
+// With expiry
+resource "gitlab_deploy_key" "example_expires" {
+  project    = "example/deploying"
+  title      = "Example deploy key"
+  key        = "ssh-ed25519 AAAA..."
+  expires_at = "2025-01-21T00:00:00Z"
 }
