5d8cab6172a52de1d50c4ca227bb7eeed2913579
Heidi Berry <heidi.berry@coop.co.uk>
2025-08-20 22:36:23 +0100

    Migrate datasource gitlab_project_variable to framework plugin

--- internal/provider/datasource_gitlab_project_variable_test.go
+++ internal/provider/datasource_gitlab_project_variable_test.go
@@ -1,15 +1,14 @@
 //go:build acceptance
 // +build acceptance
 
-package sdk
+package provider
 
 import (
 	"fmt"
 	"strconv"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"gitlab.com/gitlab-org/terraform-provider-gitlab/internal/provider/testutil"
 )
 
@@ -18,7 +17,7 @@ func TestAccDataSourceGitlabProjectVariable_basic(t *testing.T) {
 	testProjectVariable := testutil.CreateProjectVariable(t, testProject.ID)
 
 	resource.ParallelTest(t, resource.TestCase{
-		ProtoV6ProviderFactories: providerFactoriesV6,
+		ProtoV6ProviderFactories: testAccProtoV6MuxProviderFactories,
 		Steps: []resource.TestStep{
 			{
 				Config: fmt.Sprintf(`
@@ -28,6 +27,34 @@ func TestAccDataSourceGitlabProjectVariable_basic(t *testing.T) {
 						environment_scope = "%s"
 					}
 					`, testProject.ID, testProjectVariable.Key, testProjectVariable.EnvironmentScope,
+				),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.gitlab_project_variable.this", "key", testProjectVariable.Key),
+					resource.TestCheckResourceAttr("data.gitlab_project_variable.this", "value", testProjectVariable.Value),
+					resource.TestCheckResourceAttr("data.gitlab_project_variable.this", "environment_scope", testProjectVariable.EnvironmentScope),
+					resource.TestCheckResourceAttr("data.gitlab_project_variable.this", "protected", strconv.FormatBool(testProjectVariable.Protected)),
+					resource.TestCheckResourceAttr("data.gitlab_project_variable.this", "masked", strconv.FormatBool(testProjectVariable.Masked)),
+					resource.TestCheckResourceAttr("data.gitlab_project_variable.this", "variable_type", string(testProjectVariable.VariableType)),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceGitlabProjectVariable_noEnvironmentScope(t *testing.T) {
+	testProject := testutil.CreateProject(t)
+	testProjectVariable := testutil.CreateProjectVariable(t, testProject.ID)
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6MuxProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					data "gitlab_project_variable" "this" {
+						project           = %d
+						key               = "%s"
+					}
+					`, testProject.ID, testProjectVariable.Key,
 				),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.gitlab_project_variable.this", "key", testProjectVariable.Key),
