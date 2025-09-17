d30c8e1b745bc4a12f97d06eab85a21742eb4638
Heidi Berry <heidi.berry@coop.co.uk>
2025-09-17 11:04:39 +0100

    Migrate datasource gitlab_project_hooks to framework plugin

--- internal/provider/datasource_gitlab_project_hooks_test.go
+++ internal/provider/datasource_gitlab_project_hooks_test.go
@@ -1,14 +1,13 @@
 //go:build acceptance
 // +build acceptance
 
-package sdk
+package provider
 
 import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"gitlab.com/gitlab-org/terraform-provider-gitlab/internal/provider/testutil"
 )
 
@@ -17,7 +16,7 @@ func TestAccDataSourceGitlabProjectHooks_basic(t *testing.T) {
 	testHooks := testutil.CreateProjectHooks(t, testProject.ID, 25)
 
 	resource.ParallelTest(t, resource.TestCase{
-		ProtoV6ProviderFactories: providerFactoriesV6,
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
 				Config: fmt.Sprintf(`
