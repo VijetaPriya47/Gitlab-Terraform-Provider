9f2e1729bc0f952d40055b3b25e5abf428b1ea9f
Patrick Rice <patrick.rice@kingland.com>
2025-10-19 15:40:24 -0500

    Harden the protected branch test
    Changelog: IMPROVEMENTS
    Subsystem: datasource/gitlab_project_protected_branch

---
+++ internal/provider/datasource_gitlab_project_protected_branch_flakey_test.go
@@ -0,0 +1,46 @@
+//go:build flakey
+// +build flakey
+
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"gitlab.com/gitlab-org/terraform-provider-gitlab/internal/provider/testutil"
+)
+
+func TestAccDataGitlabProjectProtectedBranch_search(t *testing.T) {
+	// Create a project using default branch protection, which
+	// will protect "main" by default.
+	project := testutil.CreateProject(t)
+	branch := testutil.CreateProtectedBranches(t, project, 1)[0]
+
+	//lintignore:AT001 // Data sources don't need check destroy in their tests
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6MuxProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`				
+				data "gitlab_project_protected_branch" "test" {
+				  project_id = %d
+				  name       = "%s"
+				}
+				`, project.ID, branch.Name),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.gitlab_project_protected_branch.test",
+						"name",
+						branch.Name,
+					),
+					resource.TestCheckResourceAttr(
+						"data.gitlab_project_protected_branch.test",
+						"push_access_levels.0.access_level",
+						"maintainer",
+					),
+				),
+			},
+		},
+	})
+}
